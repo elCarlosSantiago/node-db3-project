f8ea2888ce9d15ab9e8959b42a8b6ed60a61209d
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-14 20:49:57 -0400

    2nd query

--- queries.sql
+++ queries.sql
@@ -6,6 +6,10 @@ SELECT ProductName, CategoryName
 JOIN Product p ON c.id = p.categoryId;
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT o.id, s.companyname
+    FROM orders o
+JOIN Shipper s ON o.ShipVia = s.id
+WHERE o.OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
