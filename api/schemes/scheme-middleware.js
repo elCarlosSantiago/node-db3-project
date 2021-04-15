405a1c9d5147c10c40b57c273c992e2e1e2a2c0f
CarlosSantiago Nevett <csnevett@gmail.com>
2021-04-15 10:24:27 -0400

    more formatting

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,19 +1,9 @@
 const db = require('../../data/db-config.js');
 
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
 const checkSchemeId = async (req, res, next) => {
   const { scheme_id } = req.params;
   try {
-    const scheme = await db('schemes')
-      .where('scheme_id', scheme_id)
-      .first();
+    const scheme = await db('schemes').where('scheme_id', scheme_id).first();
     if (scheme) {
       next();
     } else {
@@ -27,14 +17,6 @@ const checkSchemeId = async (req, res, next) => {
   }
 };
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
   const { scheme_name } = req.body;
   if (scheme_name === undefined || '' || typeof scheme_name !== 'string') {
@@ -44,15 +26,6 @@ const validateScheme = (req, res, next) => {
   }
 };
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
   const { instructions, step_number } = req.body;
   if (
